045b4a93c461c560523b046c058d187f4c2892f9
EchKode <48565771+echkode@users.noreply.github.com>
2022-09-06 22:07:01 +0000

    Add code

---
+++ LandingSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using HarmonyLib;
+
+namespace EchKode.PBMods.ReinforcementUnitTabs
+{
+	static class LandingSystem
+	{
+		private static List<int> unitsToRemove;
+
+		internal static void Initialize()
+		{
+			var fi = AccessTools.DeclaredField(typeof(PhantomBrigade.Combat.Systems.CombatLandingSystem), nameof(unitsToRemove));
+			unitsToRemove = (List<int>)fi.GetValue(null);
+		}
+
+		internal static void RefreshTabs()
+		{
+			if (unitsToRemove.Count == 0)
+			{
+				return;
+			}
+			CIViewCombatMode.ins.RedrawUnitTabs();
+		}
+	}
+}
---
+++ Patch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace EchKode.PBMods.ReinforcementUnitTabs
+{
+	[HarmonyPatch]
+	static class Patch
+	{
+		[HarmonyPatch(typeof(PhantomBrigade.Combat.Systems.CombatLandingSystem), "Execute")]
+		[HarmonyPostfix]
+		static void Cls_ExecutePostfix()
+		{
+			LandingSystem.RefreshTabs();
+		}
+	}
+}
